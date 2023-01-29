613e23196b7633b7e194ed01d1430815bad88961
henrisoho <bakingwithmymother@gmail.com>
2023-01-29 13:13:30 -0600

    preliminary setup for files made

---
+++ src/components/DropDown/DropDown.jsx
@@ -0,0 +1,17 @@
+// INSERT INTO "genres" ("name")
+// VALUES 
+// ('Adventure'),
+// ('Animated'),
+// ('Biographical'),
+// ('Comedy'),
+// ('Disaster'),
+// ('Drama'),
+// ('Epic'),
+// ('Fantasy'),
+// ('Musical'),
+// ('Romantic'),         --10
+// ('Science Fiction'),  --11
+// ('Space-Opera'),      --12
+// ('Superhero');       
+
+
---
+++ src/components/MovieAdd/MovieAdd.jsx
@@ -0,0 +1,13 @@
+// ### 1. Add Movie Form
+
+// This should show:
+
+// - an input field (for the movie title)
+// - an input field (for the movie poster image URL))
+// - a textarea (for the movie description)
+// - a dropdown (for the genres)
+
+// The Add Movie page should have the buttons:
+
+// - `Cancel` button, which should bring the user to the Home/List Page
+// - `Save` button, which should save these inputs in the database and bring the user to the Home/List Page (which now has the new movie)
