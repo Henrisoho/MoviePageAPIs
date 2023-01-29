00068491643ed4181cda5f4ca222c5ea0537db7f
henrisoho <bakingwithmymother@gmail.com>
2023-01-29 14:04:22 -0600

    came up with the idea to conditionally render to a new page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -9,3 +9,35 @@
 
 //  > Hint : You can make a GET request for a specific movie. Remember `req.params` and `:id`?
 // ---
+
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from "react";
+
+export default function Details({ movie }) {
+
+    const [isClicked, setIsClicked] = useState(true);
+    const handleClicked = () => {
+        if (isClicked === true) {
+            setIsClicked(false);
+        } else {
+            setIsClicked(true)
+        }
+    }
+
+    return (
+        <>
+            {isClicked ? (
+                <div>
+                    <h3>{movie.title}</h3>
+                    <img onClick={handleClicked} src={movie.poster} alt={movie.title} />
+                </div>
+            ) : (
+                <div>
+                    <h3 >{movie.title}</h3>
+                    <p>{movie.genre}</p>
+                </div>
+            )}
+        </>
+    )
+}
